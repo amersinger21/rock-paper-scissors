c0368cf02660f5950fc7beff36a10d87527d811e
amersinger21 <amersinger21@verizon.net>
2022-10-22 17:36:02 -0400

    add icons, button styling, and theme

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const computerSpace = document.querySelector("#computer-choice-area");
 const computerPoints = document.querySelector("#cscore");
+const computerChoiceImg = document.createElement("img");
 
 const selection = document.querySelectorAll(".choice-btn");
 const playerSpace = document.querySelector("#player-choice-area");
@@ -9,6 +10,10 @@ const start = document.querySelector("#begin");
 const textbox = document.querySelector("#results-space");
 const resultText = document.createElement("p");
 
+const originalStartColor = window
+	.getComputedStyle(start, null)
+	.getPropertyValue("background-color");
+
 resultText.id = "resultText";
 
 let playerChoice = "";
@@ -16,90 +21,117 @@ let computerScore = 0;
 let playerScore = 0;
 
 function getPlayerChoice() {
-  return playerChoice;
+	return playerChoice;
 }
 
 selection.forEach((item) => {
-  item.addEventListener("click", () => {
-    playerChoice = item.id;
-    playerSpace.innerText = playerChoice;
-    computerSpace.innerText = "";
-  });
+	item.addEventListener("click", () => {
+		playerChoice = item.id;
+		playerSpace.innerText = playerChoice;
+		computerSpace.innerText = "";
+	});
 });
 
 start.addEventListener("click", game);
 start.addEventListener("click", () => {
-  playerChoice = "";
+	start.style.backgroundColor = "#F47174";
+	setTimeout(changeBG, 100);
 });
 
-function getComputerChoice() {
-  let options = ["Rock", "Paper", "Scissors"];
+start.addEventListener("mousedown", () => {
+	start.style.backgroundColor = "#F47174";
+});
 
-  let choice = Math.floor(Math.random() * 3 + 0);
-  return options[choice];
+start.addEventListener("mouseup", changeBG);
+
+function changeBG() {
+	start.style.backgroundColor = originalStartColor;
+}
+
+function getComputerChoice() {
+	let options = ["Rock", "Paper", "Scissors"];
+
+	let choice = Math.floor(Math.random() * 3 + 0);
+
+	if (options[choice] === "Rock") {
+		computerChoiceImg.setAttribute(src, "./icons/stone.png");
+		return options[choice];
+	}
+
+	if (options[choice] === "Paper") {
+		computerChoiceImg.setAttribute(src, "./icons/sheet.png");
+		return options[choice];
+	}
+
+	if (options[choice] === "Scissors") {
+		computerChoiceImg.setAttribute(src, "./icons/scissors.png");
+		return options[choice];
+	}
 }
 
 function playRound(playerChoice, computerSelection) {
-  computerSpace.innerText = computerSelection;
+	computerSpace.appendChild(computerChoiceImg);
 
-  if (playerChoice.match(/rock/i)) {
-    if (computerSelection === "Rock") {
-      return "Tie!";
-    }
-    if (computerSelection === "Paper") {
-      return "You Lose! Paper beats Rock";
-    }
-    if (computerSelection === "Scissors") {
-      return "You Win! Rock beats Scissors";
-    }
-  }
+	if (playerChoice.match(/rock/i)) {
+		if (computerSelection === "Rock") {
+			return "Tie!";
+		}
+		if (computerSelection === "Paper") {
+			return "You Lose! Paper beats Rock";
+		}
+		if (computerSelection === "Scissors") {
+			return "You Win! Rock beats Scissors";
+		}
+	}
 
-  if (playerChoice.match(/paper/i)) {
-    if (computerSelection === "Rock") {
-      return "You Win! Paper beats Rock";
-    }
-    if (computerSelection === "Paper") {
-      return "Tie!";
-    }
-    if (computerSelection === "Scissors") {
-      return "You Lose! Scissors beats Paper";
-    }
-  }
+	if (playerChoice.match(/paper/i)) {
+		if (computerSelection === "Rock") {
+			return "You Win! Paper beats Rock";
+		}
+		if (computerSelection === "Paper") {
+			return "Tie!";
+		}
+		if (computerSelection === "Scissors") {
+			return "You Lose! Scissors beats Paper";
+		}
+	}
 
-  if (playerChoice.match(/scissors/i)) {
-    if (computerSelection === "Rock") {
-      return "You Lose! Rock beats Scissors";
-    }
-    if (computerSelection === "Paper") {
-      return "You Win! Scissors beats Paper";
-    }
-    if (computerSelection === "Scissors") {
-      return "Tie!";
-    }
-  } else {
-    playerSpace.innerText = "";
-    computerSpace.innerText = "";
-    return "Make a selection";
-  }
+	if (playerChoice.match(/scissors/i)) {
+		if (computerSelection === "Rock") {
+			return "You Lose! Rock beats Scissors";
+		}
+		if (computerSelection === "Paper") {
+			return "You Win! Scissors beats Paper";
+		}
+		if (computerSelection === "Scissors") {
+			return "Tie!";
+		}
+	} else {
+		playerSpace.innerText = "";
+		computerSpace.innerText = "";
+		return "Make a selection";
+	}
 }
 
 function game() {
-  const playerSelection = getPlayerChoice();
-  const computerSelection = getComputerChoice();
+	const playerSelection = getPlayerChoice();
+	const computerSelection = getComputerChoice();
 
-  let result = playRound(playerSelection, computerSelection);
+	playerChoice = "";
 
-  if (result.match(/You Win!/)) {
-    playerScore += 1;
-    playerPoints.textContent = playerScore;
-  }
+	let result = playRound(playerSelection, computerSelection);
 
-  if (result.match(/You Lose!/)) {
-    computerScore += 1;
-    computerPoints.textContent = computerScore;
-  }
+	if (result.match(/You Win!/)) {
+		playerScore += 1;
+		playerPoints.textContent = playerScore;
+	}
 
-  resultText.innerText = result;
-  textbox.appendChild(resultText);
-  return;
+	if (result.match(/You Lose!/)) {
+		computerScore += 1;
+		computerPoints.textContent = computerScore;
+	}
+
+	resultText.innerText = result;
+	textbox.appendChild(resultText);
+	return;
 }
